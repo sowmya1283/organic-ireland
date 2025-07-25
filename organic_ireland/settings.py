2dc8d8bd1f26d74dd506e5bd6b594d8ec6f599d2
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-07-25 12:36:13 +0100

    Added import os to settings file and added comment for processors request

--- organic_ireland/settings.py
+++ organic_ireland/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -59,7 +59,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',#required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
