1bd6f745aa9ececcf5ddf61b369a70ac4f81f11e
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-07-31 22:35:47 +0100

    Allauth Setup

--- organic_ireland/settings.py
+++ organic_ireland/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',  # Required by allauth
+    'allauth',
+    'allauth.account',
+    # Optional -- requires install using `django-allauth[socialaccount]`.
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -59,13 +64,35 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',#required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+# Site ID for allauth
+SITE_ID = 1 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # For development, use console backend
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # Allow login with either username or email
+ACCOUNT_EMAIL_REQUIRED = True  # Require email for account creation
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # Optional email verification
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True  # Require email confirmation during signup
+ACCOUNT_USERNAME_MIN_LENGTH = 4  # Minimum length for usernames
+LOGIN_URL = '/accounts/login/'  # URL to redirect to after login
+LOGIN_REDIRECT_URL = '/'  # URL to redirect to after login
 
 WSGI_APPLICATION = 'organic_ireland.wsgi.application'
 
